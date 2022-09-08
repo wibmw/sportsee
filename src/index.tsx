66739d9ad81888e9bc8a3bea9bb39472f486aab3
wibmw <ilaouer@outlook.com>
2022-09-08 13:41:21 +0200

    Header & SideBar

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import App from './App'
+import Header from './components/Header'
+import Home from './pages/Home'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import './style/style.css'
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Router>
+      <Header />
+      <Routes>
+        <Route path={'/'} element={<Home />} />
+        <Route path={'/'} element={<App />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+)
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SideBar from '../components/Sidebar'
+
+const Home = () => {
+  return (
+    <React.Fragment>
+      {/** *********** Home SideBar ******************/}
+      <SideBar />
+    </React.Fragment>
+  )
+}
+
+export default Home
