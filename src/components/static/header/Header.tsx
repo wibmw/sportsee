64140c177bf5fedb3973da5890325851719553b7
wibmw <ilaouer@outlook.com>
2022-10-26 12:37:21 +0200

    Header id

--- src/components/static/header/Header.tsx
+++ src/components/static/header/Header.tsx
@@ -11,12 +11,12 @@ const Header = () => {
   return (
     <header className='header'>
       {/** *********** Home Logo ******************/}
-      <NavLink to={'/12'} reloadDocument>
+      <NavLink to={'/'} reloadDocument>
         <img src={homeLogo} className='header_logo' alt='Kasa' />
       </NavLink>
       {/** *********** Nav links for all pages ******************/}
       <nav>
-        <NavLink to={'/12'} className='header_link' reloadDocument>
+        <NavLink to={'/'} className='header_link' reloadDocument>
           Accueil
         </NavLink>
         <NavLink to={'/'} className='header_link'>
